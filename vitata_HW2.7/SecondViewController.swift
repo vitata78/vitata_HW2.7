959435c7146e4798ca6ba9ae04ec8f4c8ef2cc92
Andrew Tolstov <andrew.tolstov@gmail.com>
2020-06-08 22:02:49 -0400

    initial commit

---
+++ vitata_HW2.7/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  vitata_HW2.7
+//
+//  Created by Andrew on 6/8/20.
+//  Copyright © 2020 Andrew. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
